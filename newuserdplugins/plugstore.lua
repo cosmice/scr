f1dcbc14e856003dde5d1ff3544b087f0c71605d
exceptional0 <69812978+exceptional0@users.noreply.github.com>
2022-09-20 14:31:04 +0000

    documentation, visuals

--- newuserdplugins/plugstore.lua
+++ newuserdplugins/plugstore.lua
@@ -16,14 +16,14 @@ for n,x in pairs(v) do
 print(i.."/"..n.."/"..x[2])
 end
 end
-funcs.sendnotif("cmds/pstore","internal ui. now.",5)
+funcs.sendnotif("cmds/pstore","internal ui. now.","rbxassetid://8119590978",5)
 end,desc="lists plugs in internal ui"}
 
 plug.padd={func=function(strt,plrarg,str)
 if not strt[1] or not plrarg or not pind[plrarg] or not pind[plrarg][strt[1]] then funcs.sendnotif("cmds/padd/failed","exact plug not found","",5) end
 ldplug(loadstring(game:HttpGet(pind[plrarg][strt[1]][1]))())
 funcs.sendnotif("loaded/plrarg/"..strt[1],"loaded",5)
-end,desc="adds plug from store"}
+end,desc="adds plug from store: padd base toolthing"}
 
 plug.psave={func=function(strt,plrarg,str)
 if not strt[1] or not plrarg or not pind[plrarg] or not pind[plrarg][strt[1]] then funcs.sendnotif("cmds/psave/failed","exact plug not found","",5) end
@@ -32,7 +32,7 @@ local plug=game:HttpGet(pind[plrarg][strt[1]][1])
 writefile("NonstopPeanutNovember\\"..strt[1]..".lua",plug)
 ldplug(loadstring(plug)())
 funcs.sendnotif("loaded/plrarg/"..strt[1],"loaded",5)
-end,desc="saves plug from store"}
+end,desc="saves plug from store: psave base plugstore"}
 plug.Reservedpluginname="base.plug-store" --a
 return plug
 end
