e680a7912490c132f5da415765557d5f21e167e7
exceptional0 <69812978+exceptional0@users.noreply.github.com>
2022-09-20 04:46:14 +0000

    Update gamespes.lua

--- newuserdplugins/gamespes.lua
+++ newuserdplugins/gamespes.lua
@@ -1,5 +1,22 @@
 local tb={
-[6708206173]={
+[10201298018]=function()
+local plug={
+esc={func=function()
+is_auto_enabled=not is_auto_enabled
+funcs.sendnotif("autoesc","status: "..tostring(is_auto_enabled),"rbxassetid://6678521436",5)
+end,desc="enable autoescape"};
+gg={func=function()
+local gn=workspace:FindFirstChild("gundropped")
+if gn then firetouchinterest(getchar():WaitForChild("HumanoidRootPart",20),gn,0) end end,desc="gungrab"};
+ag={func=function() gunt=not gunt
+funcs.sendnotif("auto-gun","status: "..tostring(gunt),"rbxassetid://6678521436",5) end,desc="autogg"}
+--
+Reservedpluginname="VH2R"
 }
+
+return plug
+end
+
 }
-return tb[game.PlaceId]
+tb=tb[game.PlaceId]
+return type(tb)=='function' and tb() or tb
