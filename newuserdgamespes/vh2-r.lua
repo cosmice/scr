cac493461e55d352c8dca7017a7f95055c2bacc2
exceptional0 <69812978+exceptional0@users.noreply.github.com>
2022-09-20 05:28:32 +0000

    Update vh2-r.lua

--- newuserdgamespes/vh2-r.lua
+++ newuserdgamespes/vh2-r.lua
@@ -1,8 +1,8 @@
 local plug={
-esc={func=function()
+esc={
+func=function()
 is_auto_enabled=not is_auto_enabled
-funcs.sendnotif("autoesc","status: "..tostring(is_auto_enabled),"rbxassetid://6678521436",5)
-end,desc="enable autoescape"};
+funcs.sendnotif("autoesc","status: "..tostring(is_auto_enabled),"rbxassetid://6678521436",5) end,desc="enable autoescape"};
 gg={func=function()
 local gn=workspace:FindFirstChild("gundropped")
 if gn then firetouchinterest(getchar():WaitForChild("HumanoidRootPart",20),gn,0) end end,desc="gungrab"};
