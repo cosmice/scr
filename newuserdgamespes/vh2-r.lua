f0af72184eef10cc1ac7aba52f96024b5e497b48
exceptional0 <69812978+exceptional0@users.noreply.github.com>
2022-09-20 05:30:41 +0000

    Update vh2-r.lua

--- newuserdgamespes/vh2-r.lua
+++ newuserdgamespes/vh2-r.lua
@@ -7,7 +7,7 @@ gg={func=function()
 local gn=workspace:FindFirstChild("gundropped")
 if gn then firetouchinterest(getchar():WaitForChild("HumanoidRootPart",20),gn,0) end end,desc="gungrab"};
 ag={func=function() gunt=not gunt
-funcs.sendnotif("auto-gun","status: "..tostring(gunt),"rbxassetid://6678521436",5) end,desc="autogg"}
+funcs.sendnotif("auto-gun","status: "..tostring(gunt),"rbxassetid://6678521436",5) end,desc="autogg"};
 --
 Reservedpluginname="VH2R"
 }
