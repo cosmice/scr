1a1139437d370e95ebb7a685fb403b5151c2bad1
exceptional0 <69812978+exceptional0@users.noreply.github.com>
2022-09-20 17:41:44 +0000

    Update char-manip.lua

--- newuserdplugins/char-manip.lua
+++ newuserdplugins/char-manip.lua
@@ -47,6 +47,65 @@ local plug={noclip={func=function()
 	nilvar={reserved=true,func=function(strt,nn,str,cmd,arg)
 	vars[arg]=nil
 	end};
+	antiac={desc="attempt to bypass speed checks",func=function(strt,parg)
+				local nc=0
+				local tb={index={"Changed","WalkSpeed","JumpPower","ChildAdded","ChildRemoved"},namecall={"WalkSpeed","JumpPower"},chind={"ChildAdded","ChildRemoved"}}
+				local fakechar=Instance.new("Model")
+				fakechar.Name=getchar().Name
+				local fakebp={}
+				for i,v in pairs(getchar():GetChildren()) do
+				if v:IsA("BasePart") or v:IsA("MeshPart") then
+				fakebp[v.Name]=v:Clone()
+				fakebp.Parent=fakechar
+				end
+				end
+				local fakehum=WaitForChildOfClass(getchar(),"Humanoid"):Clone()
+				fakehum:ClearAllChildren()
+				fakehum.Parent=fakechar
+				local ffind=table.find
+				--getgenv().funcs.speedbpenabled=not getgenv().funcs.speedbpenabled
+				local old_index
+				old_index = hookmetamethod(game, "__index", newcclosure(function(self,getting,...)
+				local mc=old_index(funcs.lplr,"Character")
+				if not checkcaller() and old_index(self,"ClassName")=="Humanoid" and (mc and old_index(self,"Parent")==mc ) and ffind(rawget(tb,"index"),getting) then
+				return old_index(fakehum,getting,...);
+				--[[elseif not checkcaller() and (mc~=nil and (self==mc or (old_index(self,"ClassName")=="BasePart" or old_index(self,"ClassName")=="Part" or old_index(self,"ClassName"=="MeshPart") and old_index(self,"Parent")==mc and old_index(old_index(getcallingscript(),"Parent"),"Parent")==mc))) and ffind(rawget(tb,"chind"),getting) then
+				return old_index(ffind(fakebp,old_index(self,"Name")) or self,getting,...)--]]
+				end;
+				return old_index(self,getting,...);
+				end))
+				local old_namecall
+				old_namecall = hookmetamethod(game, "__namecall", newcclosure(function(instance,meth,...)
+				local ncm,mc= getnamecallmethod(),old_index(funcs.lplr,"Character")
+				if not checkcaller() and ncm=="GetPropertyChangedSignal" and instance.ClassName=="Humanoid" and instance.Parent==mc and ffind(rawget(tb,"namecall"),meth) then
+                return old_namecall(fakehum,meth,...) -- FireServer() doesn't return anything, so usually there's no need to wait(9e9), unless you're trying to block a ban remote that crashes your game afterwards
+				end;
+				return old_namecall(instance,meth,...)
+				end))
+				--[[local old_newindex
+				old_newindex = hookmetamethod(game, "__newindex", newcclosure(function(self,ind,newval,...)
+				local mc=funcs.lplr.Character
+				if not checkcaller() and typeof(self)=="Instance" and self.ClassName=="Humanoid" and (mc and self.Parent==mc or not mc) and ffind(rawget(tb,"namecall"),ind) and type(newval)=="number" and newval<getproperties(WaitForChildOfClass(getchar(),"Humanoid"))[ind] or newval+1 then
+				return old_newindex(fakehum,ind,newval,...);
+				end;
+				return old_newindex(self,ind,newval,...);
+				end))--]]
+				local hum=WaitForChildOfClass(getchar(),"Humanoid")
+				local rgtyp=hum.RigType
+                for _, signal in pairs(getconnections(hum.Changed)) do signal:Disable() nc+=1 end
+				   for _, signal in pairs(getconnections(hum.ChildAdded)) do signal:Disable() nc+=1 end
+				   				   for _, signal in pairs(getconnections(hum.ChildRemoved)) do signal:Disable() nc+=1 end
+										for _, signal in pairs(getconnections(hum:GetPropertyChangedSignal("WalkSpeed"))) do signal:Disable() nc+=1 end
+											for _, signal in pairs(getconnections(hum:GetPropertyChangedSignal("JumpPower"))) do signal:Disable() nc+=1 end
+				--for i,v in pairs(getchar():GetChildren()) do if v:IsA("BasePart") then for _, signal in pairs(getconnections(v.ChildAdded)) do signal:Disable() nc+=1 end end end
+				--for i,v in pairs(getchar():GetChildren()) do if v:IsA("BasePart") then for _, signal in pairs(getconnections(v.ChildRemoved)) do signal:Disable() nc+=1 end end end
+				--[[if parg=="" and rgtyp==0 then
+				table.insert(tb.index,"StateChanged")
+				for _, signal in pairs(getconnections(hum.StateChanged)) do signal:Disable() nc+=1 end
+				end--]]
+				print(--[[parg~="" and rgtyp==0 and "disabled-method2: "..nc or--]] "disabled-method1: "..nc)
+				print(#getconnections(hum.Changed),#getconnections(hum:GetPropertyChangedSignal("WalkSpeed")),#getconnections(hum:GetPropertyChangedSignal("JumpPower")),#getconnections(hum.StateChanged),#getconnections(hum.ChildAdded),#getconnections(hum.ChildRemoved))
+	end};
 	--[[failed_floatt={func=function() 
 	vars.failed_floatt=not vars.failed_floatt
 	local hmnoid=getchar():FindFirstChildOfClass("Humanoid")
