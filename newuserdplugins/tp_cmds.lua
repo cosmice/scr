267e5d4068f0298cb4d67bf3f250f03ece722534
exceptional0 <69812978+exceptional0@users.noreply.github.com>
2022-09-20 14:19:08 +0000

    cdtp

--- newuserdplugins/tp_cmds.lua
+++ newuserdplugins/tp_cmds.lua
@@ -1,5 +1,12 @@
 local vars={
 }
+local maus=funcs.lplr:GetMouse()
+local function cdtp(kk)
+	if kk.UserInputType == Enum.UserInputType.MouseButton1 and funcs.uip:IsKeyDown(Enum.KeyCode.K) then
+		getchar():PivotTo(maus.Hit)
+	end
+end
+
 local plug={
 ["goto"]={func=function(strt,strd,str)
 	local thp=strd and funcs.xgetplr(strd,true)
@@ -17,6 +24,7 @@ end,desc="goto plr (arg[1])",aliases={to="goto"}},
 	end
 	vars.lgoto=nil
 end,desc="loopgoto plr (arg[1]), no arg to cancel"};
+["cdtp"]={func=function(strt,plrarg) if vars.cdtp then vars.cdtp:Disconnect() vars.cdtp=nil else vars.cdtp=funcs.uip.InputBegan:Connect(cdtp) end if plrarg~="nn" then funcs.sendnotif("cmds/toolthing/bambi.exe",vars.cdtp and "hi" or "fucking idiot","rbxassetid://8119590978",4) end end,desc="toggle click tp (k+click)"};
 Reservedpluginname="base.tp-cmds"
 }
 return plug
