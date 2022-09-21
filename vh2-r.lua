ec6dacd9d6a27bdd9681e3c476fba987fa46931d
exceptional0 <69812978+exceptional0@users.noreply.github.com>
2022-09-21 01:34:03 +0000

    Update vh2-r.lua

--- vh2-r.lua
+++ vh2-r.lua
@@ -1,3 +1,4 @@
  getgenv().sus_cmds={}
 if not isfolder("November") or not isfile("November\\icepoolshook.lua") then sus_cmds.ExtraPlugins={'https://raw.githubusercontent.com/exceptional0/scr/main/newuserdplugins/icepoolshook.lua'} end
+if not isfolder("November") or not isfile("November\\tp_cmds.lua") then sus_cmds.ExtraPlugins={'https://raw.githubusercontent.com/exceptional0/scr/main/newuserdplugins/tp_cmds.lua'} end
 loadstring(game:HttpGet("https://raw.githubusercontent.com/exceptional0/scr/main/hughjazz.lua"))()
