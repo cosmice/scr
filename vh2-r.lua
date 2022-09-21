ac155744ee74b66d7add8013f2542017f4ff4dab
exceptional0 <69812978+exceptional0@users.noreply.github.com>
2022-09-21 03:18:58 +0000

    Update vh2-r.lua

--- vh2-r.lua
+++ vh2-r.lua
@@ -1,5 +1,6 @@
- getgenv().sus_cmds={}
-if not isfolder("November") or not isfile("November\\icepoolshook.lua") then sus_cmds.ExtraPlugins={'https://raw.githubusercontent.com/exceptional0/scr/main/newuserdplugins/icepoolshook.lua'} end
-if not isfolder("November") or not isfile("November\\tp_cmds.lua") then sus_cmds.ExtraPlugins={'https://raw.githubusercontent.com/exceptional0/scr/main/newuserdplugins/tp_cmds.lua'} end
-if not isfolder("November") or not isfile("November\\char-manip.lua") then sus_cmds.ExtraPlugins={'https://raw.githubusercontent.com/exceptional0/scr/main/newuserdplugins/char-manip.lua'} end
+getgenv().sus_cmds={}
+sus_cmds.ExtraPlugins={}
+if not isfolder("November") or not isfile("November\\icepoolshook.lua") then table.insert(sus_cmds.ExtraPlugins,'https://raw.githubusercontent.com/exceptional0/scr/main/newuserdplugins/icepoolshook.lua') end
+if not isfolder("November") or not isfile("November\\tp_cmds.lua") then table.insert(sus_cmds.ExtraPlugins,'https://raw.githubusercontent.com/exceptional0/scr/main/newuserdplugins/tp_cmds.lua') end
+if not isfolder("November") or not isfile("November\\char-manip.lua") then table.insert(sus_cmds.ExtraPlugins,'https://raw.githubusercontent.com/exceptional0/scr/main/newuserdplugins/char-manip.lua') end
 loadstring(game:HttpGet("https://raw.githubusercontent.com/exceptional0/scr/main/hughjazz.lua"))()
