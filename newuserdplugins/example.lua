93c521e13fd6e0460534aabd61c5db3ca9599037
exceptional0 <69812978+exceptional0@users.noreply.github.com>
2022-09-22 09:59:25 +0000

    Update example.lua

--- newuserdplugins/example.lua
+++ newuserdplugins/example.lua
@@ -2,12 +2,12 @@ local example_plug={
 expo={func=function(a,aaa,aaaa) funcs.sendnotif("cmds/example",aaaa,"rbxassetid://8119590978",5) end,desc="example: notify args"}
 }
 funcs.sendnotif(
-powersupply and powersupply.gnn and "powered: gnn" or "false",
+powersupply and powersupply.gnn and "powered: gnn" or "false", --gui/gooey
 "powersupply",
 "",
 5)
 funcs.sendnotif(
-powersupply and powersupply.cmds and "powered: cmds" or "false",
+powersupply and powersupply.cmds and "powered: cmds" or "false", --cmdstb
 "powersupply",
 "",
 5)
