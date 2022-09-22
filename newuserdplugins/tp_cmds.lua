45f7636757dfcb1071c5c33c20a187a742369eab
exceptional0 <69812978+exceptional0@users.noreply.github.com>
2022-09-22 16:21:43 +0000

    2-3 new cmds

--- newuserdplugins/tp_cmds.lua
+++ newuserdplugins/tp_cmds.lua
@@ -34,8 +34,10 @@ end,desc="goto arg[1] plr and back arg[2] amount of times for arg[3] each"};
 	end
 	vars.lgoto=nil
 end,desc="loopgoto plr (arg[1]), no arg to cancel"};
-["b"]={["func"]=function(strt,nn,str,cmd,arg) nn=tonumber(nn) or 2 for i = 1,30 do local piv=getchar():GetPivot() getchar():PivotTo(piv + (-piv.LookVector * nn/4)) ; piv=getchar():GetPivot() getchar():PivotTo(piv + (-piv.LookVector * nn/4)) task.wait(0) end end;["desc"]="tp backwards arg[1] or 6"};
-["f"]={["func"]=function(strt,nn,str,cmd,arg) nn=tonumber(nn) or 2 for i = 1,30 do local piv=getchar():GetPivot() getchar():PivotTo(piv + (piv.LookVector * nn/4)) ; piv=getchar():GetPivot() getchar():PivotTo(piv + (piv.LookVector * nn/4))  task.wait(0) end end;["desc"]="tp forwards arg[1] or 6"};
+["tovoid"]={["func"]=function() vars.outvoid=vars.outvoid or getchar():GetPivot() ; getchar():PivotTo(CFrame.new(Random.new():NextNumber(80000000000,100000000000),Random.new():NextNumber(80000000000,100000000000),Random.new():NextNumber(80000000000,100000000000))) end;["desc"]="tp yourself to void"};
+["outvoid"]={["func"]=function() if vars.outvoid then getchar():PivotTo(vars.outvoid) vars.outvoid=nil end end;["desc"]="tp where you were before tping to void"};
+["b"]={["func"]=function(strt,nn,str,cmd,arg) nn=tonumber(nn) or 1.5 cmd=getchar() for i = 1,30 do if not funcs.lplr.Character==cmd then break end local piv=cmd:GetPivot() cmd:PivotTo(piv + (-piv.LookVector * nn/4)) ; piv=cmd:GetPivot() cmd:PivotTo(piv + (-piv.LookVector * nn/4)) task.wait(0) end end;["desc"]="tp backwards arg[1] or 6"};
+["f"]={["func"]=function(strt,nn,str,cmd,arg) nn=tonumber(nn) or 1.5 cmd=getchar() for i = 1,30 do if not funcs.lplr.Character==cmd then break end local piv=cmd:GetPivot() cmd:PivotTo(piv + (piv.LookVector * nn/4)) ; piv=cmd:GetPivot() cmd:PivotTo(piv + (piv.LookVector * nn/4))  task.wait(0) end end;["desc"]="tp forwards arg[1] or 6"};
 ["cdtp"]={func=function(strt,plrarg) if vars.cdtp then vars.cdtp:Disconnect() vars.cdtp=nil else vars.cdtp=funcs.uip.InputBegan:Connect(cdtp) end if plrarg~="nn" then funcs.sendnotif("cmds/toolthing/bambi.exe",vars.cdtp and "hi" or "fucking idiot","rbxassetid://8119590978",4) end end,desc="toggle click tp (k+click)"};
 Reservedpluginname="base.tp-cmds"
 }
