e1729cda6b95b7d3844d9e8451c14273be1e0c28
exceptional0 <69812978+exceptional0@users.noreply.github.com>
2022-09-22 02:29:21 +0000

    protectgui+gui is placed within a table

--- hughjazz.lua
+++ hughjazz.lua
@@ -2,81 +2,82 @@
 if not funcs then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/exceptional0/scr/main/funcs.lua"))()
 end
-local main = Instance.new("ScreenGui")
-local _txtbox = Instance.new("TextBox")
-local cmdframe = Instance.new("ScrollingFrame")
-local txt = Instance.new("TextLabel")
-local _close = Instance.new("TextButton")
-local lplr = funcs.lplr
+local gnn={
+main = Instance.new("ScreenGui");
+_txtbox = Instance.new("TextBox");
+cmdframe = Instance.new("ScrollingFrame");
+txt = Instance.new("TextLabel");
+_close = Instance.new("TextButton");
+event = Instance.new("BindableEvent");
+gprot = gethui or get_hidden_ui or get_hidden_gui or hiddenUI or syn and syn.protectgui and (function(x) syn.protectgui(x) return game:GetService("CoreGui") end) or function() return game:GetService("CoreGui") end}
+gnn.main.Name = "main"
+gnn.main.Parent = gnn.gprot(x)
+gnn.main.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+gnn._txtbox.Name = "_txtbox"
+gnn._txtbox.Parent = gnn.main
+gnn._txtbox.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+gnn._txtbox.BackgroundTransparency = 0.350
+gnn._txtbox.Draggable = true
+gnn._txtbox.Position = UDim2.new(0.61360544, 0, 0.0637362674, 0)
+gnn._txtbox.Size = UDim2.new(0.136734694, 0, 0.0802197829, 0)
+gnn._txtbox.Font = Enum.Font.SourceSans
+gnn._txtbox.PlaceholderText = "cmds r sus"
+gnn._txtbox.Text = ""
+gnn._txtbox.TextColor3 = Color3.fromRGB(255, 255, 255)
+gnn._txtbox.TextScaled = true
+gnn._txtbox.TextSize = 14.000
+gnn._txtbox.TextWrapped = true
 
-main.Name = "main"
-main.Parent = game:GetService("CoreGui")
-main.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-_txtbox.Name = "_txtbox"
-_txtbox.Parent = main
-_txtbox.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-_txtbox.BackgroundTransparency = 0.350
-_txtbox.Draggable = true
-_txtbox.Position = UDim2.new(0.61360544, 0, 0.0637362674, 0)
-_txtbox.Size = UDim2.new(0.136734694, 0, 0.0802197829, 0)
-_txtbox.Font = Enum.Font.SourceSans
-_txtbox.PlaceholderText = "cmds r sus"
-_txtbox.Text = ""
-_txtbox.TextColor3 = Color3.fromRGB(255, 255, 255)
-_txtbox.TextScaled = true
-_txtbox.TextSize = 14.000
-_txtbox.TextWrapped = true
+gnn.cmdframe.Name = "cmdframe"
+gnn.cmdframe.Active = false
+gnn.cmdframe.Parent = gnn.main
+gnn.cmdframe.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+gnn.cmdframe.Position = UDim2.new(0.376870751, 0, 0.309890121, 0)
+gnn.cmdframe.Size = UDim2.new(0.245578229, 0, 0.376923114, 0)
+gnn.cmdframe.Visible = false
+gnn.cmdframe.CanvasSize = UDim2.new(0, 0, 0.529999971, 0)
+gnn.cmdframe.Draggable=true
+gnn.cmdframe.AutomaticCanvasSize=3
+gnn.cmdframe.CanvasSize=UDim2.new(0.5,0,0,0)
 
-cmdframe.Name = "cmdframe"
-cmdframe.Active = false
-cmdframe.Parent = main
-cmdframe.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-cmdframe.Position = UDim2.new(0.376870751, 0, 0.309890121, 0)
-cmdframe.Size = UDim2.new(0.245578229, 0, 0.376923114, 0)
-cmdframe.Visible = false
-cmdframe.CanvasSize = UDim2.new(0, 0, 0.529999971, 0)
-cmdframe.Draggable=true
-cmdframe.AutomaticCanvasSize=3
-cmdframe.CanvasSize=UDim2.new(0.5,0,0,0)
-
-txt.Name = "txt"
-txt.Parent = cmdframe
-txt.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-txt.BackgroundTransparency = 1.000
-txt.Position = UDim2.new(0, 0, -0.000313895056, 0)
-txt.Size = UDim2.new(0.963989615, 0, 1.00031388, 0)
-txt.Font = Enum.Font.SourceSans
-txt.Text = [[commands:
+gnn.txt.Name = "txt"
+gnn.txt.Parent = gnn.cmdframe
+gnn.txt.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+gnn.txt.BackgroundTransparency = 1.000
+gnn.txt.Position = UDim2.new(0, 0, -0.000313895056, 0)
+gnn.txt.Size = UDim2.new(0.963989615, 0, 1.00031388, 0)
+gnn.txt.Font = Enum.Font.SourceSans
+gnn.txt.Text = [[commands:
 rj
 plugin commands:
 ]]
-txt.TextColor3 = Color3.fromRGB(255, 255, 255)
-txt.TextSize = 14.000
-txt.TextXAlignment = Enum.TextXAlignment.Left
-txt.TextYAlignment = Enum.TextYAlignment.Top
-txt.AutomaticSize=3
-local txt2=txt:Clone()
-txt2.Parent=cmdframe
-txt2.Text=""
-txt2.Visible=false txt2.Active=false
-_close.Name = "_close"
-_close.Parent = main
-_close.Active = false
-_close.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-_close.Position = UDim2.new(0.376870751, 0, 0.686813176, 0)
-_close.Size = UDim2.new(0.245578229, 0, 0.0582417585, 0)
-_close.Visible = false
-_close.Font = Enum.Font.SourceSans
-_close.Text = "close"
-_close.TextColor3 = Color3.fromRGB(255, 255, 255)
-_close.TextSize = 14.000
+gnn.txt.TextColor3 = Color3.fromRGB(255, 255, 255)
+gnn.txt.TextSize = 14.000
+gnn.txt.TextXAlignment = Enum.TextXAlignment.Left
+gnn.txt.TextYAlignment = Enum.TextYAlignment.Top
+gnn.txt.AutomaticSize=3
+gnn.txt2=gnn.txt:Clone()
+gnn.txt2.Parent=gnn.cmdframe
+gnn.txt2.Text=""
+gnn.txt2.Visible=false gnn.txt2.Active=false
+gnn._close.Name = "_close"
+gnn._close.Parent = gnn.main
+gnn._close.Active = false
+gnn._close.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+gnn._close.Position = UDim2.new(0.376870751, 0, 0.686813176, 0)
+gnn._close.Size = UDim2.new(0.245578229, 0, 0.0582417585, 0)
+gnn._close.Visible = false
+gnn._close.Font = Enum.Font.SourceSans
+gnn._close.Text = "close"
+gnn._close.TextColor3 = Color3.fromRGB(255, 255, 255)
+gnn._close.TextSize = 14.000
 local cmds=sus_cmds or {}
 cmds["cmds"]=cmds["cmds"] or function()
-		_close.Active = true
-		cmdframe.Active = true
-		_close.Visible = true
-		cmdframe.Visible = true
-		txt.Visible=true txt.Active=true txt2.Visible=false txt2.Active=false
+		gnn._close.Active = true
+		gnn.cmdframe.Active = true
+		gnn._close.Visible = true
+		gnn.cmdframe.Visible = true
+		gnn.txt.Visible=true gnn.txt.Active=true gnn.txt2.Visible=false gnn.txt2.Active=false
 end
 cmds["rj"]=cmds["rj"] or rj
 local aliases={}
@@ -94,42 +95,41 @@ cmds[cmd][1](strd,plrarg,x,cmd,cmds[cmd][2])
 end
 
 end
-local function OVTEMO_fake_script() -- _txtbox.txt 
+local function OVTEMO_fake_script() -- gnn._txtbox.txt 
 	local script = Instance.new('LocalScript', _txtbox)
 
 	local function onfocus(x)
 		if x then
-			cmd(_txtbox.Text:lower())
+			cmd(gnn._txtbox.Text:lower())
 		end
 	end
-	_txtbox.FocusLost:Connect(onfocus)
+	gnn._txtbox.FocusLost:Connect(onfocus)
 end
 coroutine.wrap(OVTEMO_fake_script)()
-local function ZURHG_fake_script() -- _close.LocalScript 
+local function ZURHG_fake_script() -- gnn._close.LocalScript 
 	local script = Instance.new('LocalScript', _close)
 	local function onclick()
-		_close.Active = not _close.Active
-		cmdframe.Active = not cmdframe.Active
-		_close.Visible = not _close.Visible
-		cmdframe.Visible = not cmdframe.Visible
-		txt.Visible=false txt.Active=false txt2.Visible=false txt2.Active=false
+		gnn._close.Active = not gnn._close.Active
+		gnn.cmdframe.Active = not gnn.cmdframe.Active
+		gnn._close.Visible = not gnn._close.Visible
+		gnn.cmdframe.Visible = not gnn.cmdframe.Visible
+		gnn.txt.Visible=false gnn.txt.Active=false txt2.Visible=false txt2.Active=false
 	end
-	_close.MouseButton1Click:Connect(onclick)
+	gnn._close.MouseButton1Click:Connect(onclick)
 end
 coroutine.wrap(ZURHG_fake_script)()
-local us = game:GetService("UserInputService")
 local function stfu()
-_txtbox.Text = ""
+gnn._txtbox.Text = ""
 end
 local function onkeydown(x)
-local txtfocused = us:GetFocusedTextBox()
+local txtfocused = funcs.uip:GetFocusedTextBox()
 if txtfocused then return end
 if x.KeyCode == Enum.KeyCode.BackSlash then
-_txtbox:CaptureFocus()
+gnn._txtbox:CaptureFocus()
 task.defer(stfu,"")
 end
 end
-us.InputBegan:Connect(onkeydown)
+funcs.uip.InputBegan:Connect(onkeydown)
 
 local function ldplug(v)
 local tnstr={}
@@ -137,7 +137,7 @@ local function HandlePluginError(err)
 printconsole(v.."- "..err,171,199,80)
 end
 local ldfile
-local function pl() ldfile=type(v)=='table' and v or loadfile(v)() if ldfile and ldfile.Init then ldfile=ldfile.Init(HandlePluginError,ldplug,{["txt2"]=txt2,["txt"]=txt,["cmdframe"]=cmdframe,["_close"]=_close}) end end
+local function pl() ldfile=type(v)=='table' and v or loadfile(v)() if ldfile and ldfile.Init then ldfile=ldfile.Init(HandlePluginError,ldplug,gnn) end end
 xpcall(pl,HandlePluginError)
 if not ldfile then return end
 local nm=ldfile.Reservedpluginname or v
@@ -159,7 +159,7 @@ aliases[i]=v
 end
 end
 end
-txt.Text=txt.Text..'\n'..table.concat(tnstr,"\n").."\n"
+gnn.txt.Text=gnn.txt.Text..'\n'..table.concat(tnstr,"\n").."\n"
 end
 
 
@@ -167,13 +167,13 @@ end
 if isfolder("November") then
 for i,v in pairs(listfiles("November")) do
 task.spawn(ldplug,v)
-task.wait()
+task.wait(0)
 end
 end
 if cmds.ExtraPlugins then
 for i,v in pairs(cmds.ExtraPlugins) do
 task.spawn(ldplug,type(v)=='string' and loadstring(game:HttpGet(v))() or v)
-task.wait()
+task.wait(0)
 end
 end
 getgenv().sus_cmds=nil
