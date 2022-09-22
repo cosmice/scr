3e785024040e3dd1a8e8c044bd07332600b84683
exceptional0 <69812978+exceptional0@users.noreply.github.com>
2022-09-22 09:58:59 +0000

    Update example.lua

--- newuserdplugins/example.lua
+++ newuserdplugins/example.lua
@@ -1,4 +1,14 @@
 local example_plug={
-expo={func=function(a,aaa,aaaa) funcs.sendnotif("cmds/pstore",aaaa,"rbxassetid://8119590978",5) end,desc="example: notify args"}
+expo={func=function(a,aaa,aaaa) funcs.sendnotif("cmds/example",aaaa,"rbxassetid://8119590978",5) end,desc="example: notify args"}
 }
+funcs.sendnotif(
+powersupply and powersupply.gnn and "powered: gnn" or "false",
+"powersupply",
+"",
+5)
+funcs.sendnotif(
+powersupply and powersupply.cmds and "powered: cmds" or "false",
+"powersupply",
+"",
+5)
 return example_plug
