b5629f3130c34cfa86ee7965daf8702aa027c6ca
exceptional0 <69812978+exceptional0@users.noreply.github.com>
2022-09-25 18:30:19 +0000

    Update fpscap69.lua

--- fpscap69.lua
+++ fpscap69.lua
@@ -3,11 +3,14 @@ getgenv().fpsthing = {}
 getgenv().fpsthing.maxfpswhen = 65
 getgenv().fpsthing.minfpswhen = 22
 setfpscap(fpsthing.maxfpswhen)
+local oldsetts={}
 local uis = game:GetService("UserInputService")
 local con1,con2 = nil,nil
 local ru=game:GetService("RunService")
 con1 = uis.WindowFocused:Connect(function()
-	if getgenv().fpsthingenabled == true then
+	if getgenv().fpsthingenabled == true and #oldsetts>=2 then
+	settings().Rendering.QualityLevel=oldsetts[2]
+	settings().Physics.PhysicsEnvironmentalThrottle=oldsetts[1]
 	ru:Set3dRenderingEnabled(true)
     setfpscap(getgenv().fpsthing.maxfpswhen)
 	--[[else
@@ -21,6 +24,10 @@ con2 = uis.WindowFocusReleased:Connect(function()
 	if getgenv().fpsthingenabled == true then
     setfpscap(getgenv().fpsthing.minfpswhen)
 	ru:Set3dRenderingEnabled(false)
+	oldsetts[1]=settings().Physics.PhysicsEnvironmentalThrottle
+	settings().Physics.PhysicsEnvironmentalThrottle = 1
+	oldsetts[2]=settings().Rendering.QualityLevel
+	settings().Rendering.QualityLevel = 'Level01'
 	--[[else
 	con1:Disconnect()
 	con2:Disconnect()
