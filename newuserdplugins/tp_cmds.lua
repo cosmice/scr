22cc393b24289df726f4caa9e4d24b33ba84e1f0
exceptional0 <69812978+exceptional0@users.noreply.github.com>
2022-09-26 01:46:05 +0000

    Update tp_cmds.lua

--- newuserdplugins/tp_cmds.lua
+++ newuserdplugins/tp_cmds.lua
@@ -3,7 +3,7 @@ local vars={
 local maus=funcs.lplr:GetMouse()
 local function cdtp(kk)
 	if kk.UserInputType == Enum.UserInputType.MouseButton3 and funcs.uip:IsKeyDown(Enum.KeyCode.K) then
-		getchar():PivotTo(maus.Hit)
+		getchar():PivotTo(maus.Hit*CFrame.new(0,2.45,0))
 	end
 end
 local function togo(strt,strd,str)
@@ -34,11 +34,11 @@ end,desc="goto arg[1] plr and back arg[2] amount of times for arg[3] each"};
 	end
 	vars.lgoto=nil
 end,desc="loopgoto plr (arg[1]), no arg to cancel"};
-["tovoid"]={["func"]=function() vars.outvoid=vars.outvoid or getchar():GetPivot() ; getchar():PivotTo(CFrame.new(Random.new():NextNumber(80000000000,100000000000),Random.new():NextNumber(80000000000,100000000000),Random.new():NextNumber(80000000000,100000000000))) end;["desc"]="tp yourself to void"};
-["outvoid"]={["func"]=function() if vars.outvoid then getchar():PivotTo(vars.outvoid) vars.outvoid=nil end end;["desc"]="tp where you were before tping to void"};
+["tovoid"]={["func"]=function() vars.outvoid=vars.outvoid or getchar():GetPivot() ; local prr=getchar().PrimaryPart or getchar():GetPropertyChangedSignal("PrimaryPart"):Wait() ; if prr then prr.Anchored=true end getchar():PivotTo(CFrame.new(Random.new():NextNumber(80000000000,100000000000),Random.new():NextNumber(80000000000,100000000000),Random.new():NextNumber(80000000000,100000000000))) end;["desc"]="tp yourself to void, anchors your root too"};
+["outvoid"]={["func"]=function() if vars.outvoid then  local prr=getchar().PrimaryPart or getchar():GetPropertyChangedSignal("PrimaryPart"):Wait() ; if prr then prr.Anchored=false end getchar():PivotTo(vars.outvoid) vars.outvoid=nil end end;["desc"]="tp where you were before tping to void, unanchors your root too"};
 ["b"]={["func"]=function(strt,nn,str,cmd,arg) nn=tonumber(nn) or 1.5 cmd=getchar() for i = 1,30 do if not funcs.lplr.Character==cmd then break end local piv=cmd:GetPivot() cmd:PivotTo(piv + (-piv.LookVector * nn/4)) ; piv=cmd:GetPivot() cmd:PivotTo(piv + (-piv.LookVector * nn/4)) task.wait(0) end end;["desc"]="tp backwards arg[1] or 6"};
 ["f"]={["func"]=function(strt,nn,str,cmd,arg) nn=tonumber(nn) or 1.5 cmd=getchar() for i = 1,30 do if not funcs.lplr.Character==cmd then break end local piv=cmd:GetPivot() cmd:PivotTo(piv + (piv.LookVector * nn/4)) ; piv=cmd:GetPivot() cmd:PivotTo(piv + (piv.LookVector * nn/4))  task.wait(0) end end;["desc"]="tp forwards arg[1] or 6"};
-["cdtp"]={func=function(strt,plrarg) if vars.cdtp then vars.cdtp:Disconnect() vars.cdtp=nil else vars.cdtp=funcs.uip.InputBegan:Connect(cdtp) end if plrarg~="nn" then funcs.sendnotif("cmds/toolthing/bambi.exe",vars.cdtp and "hi" or "fucking idiot","rbxassetid://8119590978",4) end end,desc="toggle click tp (k+click)"};
+["cdtp"]={func=function(strt,plrarg) if vars.cdtp then vars.cdtp:Disconnect() vars.cdtp=nil else vars.cdtp=funcs.uip.InputBegan:Connect(cdtp) end if plrarg~="nn" then funcs.sendnotif("cmds/toolthing/bambi.exe",vars.cdtp and "hi" or "fucking idiot","rbxassetid://8119590978",4) end end,desc="toggle click tp (k+middleclick)"};
 Reservedpluginname="base.tp-cmds"
 }
 return plug
