b297281a82b4ead4c9d873ef402bab5404cbc20c
6yNuiC9 <69812978+6yNuiC9@users.noreply.github.com>
2022-10-06 06:44:52 +0000

    Create rv-theme.lua

---
+++ newuserdplugins/rv-theme.lua
@@ -0,0 +1,3 @@
+powersupply.gnn._txtbox.BackgroundColor3=Color3.fromRGB(76, 25, 25)
+powersupply.gnn._txtbox:SetAttribute('BackgroundColor3',powersupply.gnn._txtbox.BackgroundColor3)
+powersupply.gnn._txtbox:SetAttribute('BackgroundColor',powersupply.gnn._txtbox.BackgroundColor)
