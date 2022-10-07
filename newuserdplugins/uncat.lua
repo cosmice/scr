ffc823eae54f77d8691adb362e7b340743e583c0
6yNuiC9 <69812978+6yNuiC9@users.noreply.github.com>
2022-10-07 04:58:37 +0000

    fix + cooldown leniency + eyesore removal

--- newuserdplugins/uncat.lua
+++ newuserdplugins/uncat.lua
@@ -8,12 +8,12 @@ vars.loops={}
 
 	
 local plug={
-["msgb"]={["func"]=function() getgenv().msgb=not getgenv().msgb funcs.sendnotif("uncat/msgb",getgenv().msgb and ":flushed:" or "dumbfuck","",8) end};
-["ftouchinterests"]={["func"]=function(strt,nn) nn=nn and funcs.xgetplr(nn,true) or funcs.lplr ; for i,v in pairs(workspace:GetDescendants()) do if v:IsA("TouchTransmitter") then task.spawn(vars.funcs.ft,v,nn) end if i%3 then task.wait() end end end;["desc"]="dumbass";["aliases"]={["firetouchinterests"]='ftouchinterests';["fti"]="ftouchinterests"}};
+["msgb"]={["func"]=function() getgenv().msgb=not getgenv().msgb funcs.sendnotif("uncat\\msgb",getgenv().msgb and ":flushed:" or "dumbfuck","",8) end};
+["ftouchinterests"]={["func"]=function(strt,nn) nn=nn and funcs.xgetplr(nn,true) or funcs.lplr ; for i,v in pairs(workspace:GetDescendants()) do if v:IsA("TouchTransmitter") then task.spawn(vars.funcs.ft,v,nn) end if i%6==0 then task.wait() end end end;["desc"]="dumbass";["aliases"]={["firetouchinterests"]='ftouchinterests';["fti"]="ftouchinterests"}};
 ["fcd"]={["func"]=function() 
 for i,v in pairs(workspace:GetDescendants()) do
 if v:IsA("ClickDetector") then 
-fireclickdetector(v) end if i%3==0 then task.wait() end 
+fireclickdetector(v) end if i%6==0 then task.wait() end 
 end 
 end;["desc"]="firecd"};
 ["notifcmd"]={["func"]=function(n,n,thud)
