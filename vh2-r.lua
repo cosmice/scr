bc6ffaf1db7feb9b33dabeed9ec0a557504a225e
6yNuiC9 <69812978+6yNuiC9@users.noreply.github.com>
2022-10-08 08:23:35 +0000

    Update vh2-r.lua

--- vh2-r.lua
+++ vh2-r.lua
@@ -3,4 +3,4 @@ sus_cmds.ExtraPlugins={}
 if not isfolder("November") or not isfile("November\\icepoolshook.lua") then table.insert(sus_cmds.ExtraPlugins,'https://raw.githubusercontent.com/6yNuiC9/scr/main/newuserdplugins/icepoolshook.lua') end
 if not isfolder("November") or not isfile("November\\tp_cmds.lua") then table.insert(sus_cmds.ExtraPlugins,'https://raw.githubusercontent.com/6yNuiC9/scr/main/newuserdplugins/tp_cmds.lua') end
 if not isfolder("November") or not isfile("November\\char-manip.lua") then table.insert(sus_cmds.ExtraPlugins,'https://raw.githubusercontent.com/6yNuiC9/scr/main/newuserdplugins/char-manip.lua') end
-loadstring(game:HttpGet("https://raw.githubusercontent.com/exceptional0/scr/main/hughjazz.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/6yNuiC9/scr/main/hughjazz.lua"))()
