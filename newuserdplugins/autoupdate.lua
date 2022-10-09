2affde53429f7c533351510102277d2cf1968df4
6yNuiC9 <69812978+6yNuiC9@users.noreply.github.com>
2022-10-09 00:02:43 +0000

    fix

--- newuserdplugins/autoupdate.lua
+++ newuserdplugins/autoupdate.lua
@@ -1,4 +1,12 @@
+task.spawn(function()
 if not powersupply.gnn.Plugsloaded then powersupply.gnn.plugsloaded.Event:Wait() end
-if powersupply.cmds['pgsave'] then
+if powersupply.cmds['pgsave'] and not getgenv().sus_cmds_autoupdating then
+getgenv().sus_cmds_autoupdating=true
 powersupply.cmds['pgsave'][1](nil,'base')
+loadstring(game:HttpGet("https://raw.githubusercontent.com/6yNuiC9/scr/main/hughjazz.lua"))()
+powersupply.cmds['gclose']()
+elseif getgenv().sus_cmds_autoupdating then getgenv().sus_cmds_autoupdating=nil
+funcs.sendnotif('uwu','uwu','rbxassetid://9231004719',8)
 end
+end)
+return {}
