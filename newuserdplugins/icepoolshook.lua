e552865ebf1a48f0a34a5bf333642fc7f6ab8b2d
6yNuiC9 <69812978+6yNuiC9@users.noreply.github.com>
2022-10-13 08:12:05 +0000

    Update icepoolshook.lua

--- newuserdplugins/icepoolshook.lua
+++ newuserdplugins/icepoolshook.lua
@@ -1,5 +1,5 @@
 local tb={
-[10201298018]='https://raw.githubusercontent.com/exceptional0/scr/main/newuserdgamespes/vh2-r.lua';
+[10201298018]='https://raw.githubusercontent.com/6yNuiC9/scr/main/newuserdgamespes/vh2-r.lua';
 [198817751]='https://raw.githubusercontent.com/6yNuiC9/scr/main/newuserdgamespes/vh2.lua'
 }
 tb=tb[game.PlaceId]
