e294e84e65e7df9d2b2a72a5fd4df30b02b551c7
6yNuiC9 <69812978+6yNuiC9@users.noreply.github.com>
2022-10-13 05:25:28 +0000

    Update vh2.lua

--- newuserdgamespes/vh2.lua
+++ newuserdgamespes/vh2.lua
@@ -1,25 +1,26 @@
 local vars={['gdata']=workspace.GameData.Players}
 vars.funcs={}
-vars.funcs.chck=function(val)
-local theplr=funcs.plrs:FindFirstChild(val.Name)
-if not theplr or not theplr:IsA('Player') or not theplr.Character or not val:FindFirstChild('Mode') then return end
+vars.funcs.chck=function(val) task.wait(1)
+local theplr=funcs.plrs:WaitForChild(val.Name,3)
+if not theplr or not theplr:IsA('Player') or not val:WaitForChild('Mode',3) then return end
+local thech=getchar(nil,nil,theplr)
+if not thech then return end
 local hk
-local con
-local con2
+local cons={}
 if val.Mode.Value==3 then
-hk=funcs.addhook(theplr.Character,{['color']=Color3.fromRGB(100,0,0);['txt']=theplr.Name..' is a vampire..';['job']='vhesp';['dep']={theplr.CharacterRemoving}})
+hk=funcs.addhook(thech:WaitForChild('Head',2),{['color']=Color3.fromRGB(100,0,0);['txt']=theplr.Name..' is a vampire..';['job']='vhesp';['dep']={theplr.CharacterRemoving}})
 elseif val.Mode.Value==2 then
-hk=funcs.addhook(theplr.Character,{['txt']=theplr.Name..' is a detective..';['job']='vhesp';['dep']={theplr.CharacterRemoving}})
+hk=funcs.addhook(thech:WaitForChild('Head',2),{['txt']=theplr.Name..' is a detective..';['job']='vhesp';['dep']={theplr.CharacterRemoving}})
 end
 if hk then
-local desfunc=function()
-if con then con:Disconnect() con=nil end if con2 then con2:Disconnect() con2=nil end if hk and hk.justquit then hk.justquit() end if val and val.Parent then vars.funcs.chck(val) end
+local function desfunc()
+for i,v in pairs(cons) do if v then v:Disconnect() end end cons=nil if hk and hk.justquit then hk.justquit() end if val and val.Parent then vars.funcs.chck(val) end desfunc=nil
 end
-con=val.Mode.Changed:Connect(desfunc)
-con2=val.Mode.Destroying:Connect(desfunc)
-end
-
+cons[1]=val.Mode.Changed:Connect(desfunc)
+cons[2]=val.ChildAdded:Connect(desfunc)
 end
 
-vars.gdata.ChildAdded:Connect(vars.funcs.chck)
+end
+
 for i,v in pairs(vars.gdata:GetChildren()) do vars.funcs.chck(v) end
+vars.gdata.ChildAdded:Connect(vars.funcs.chck)
