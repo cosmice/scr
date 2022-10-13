0f722fec7f37ec86fa3a5c810aa359acd33f3d6b
6yNuiC9 <69812978+6yNuiC9@users.noreply.github.com>
2022-10-13 04:25:07 +0000

    vh2

--- newuserdplugins/icepoolshook.lua
+++ newuserdplugins/icepoolshook.lua
@@ -1,21 +1,7 @@
 local tb={
-[10201298018]='https://raw.githubusercontent.com/exceptional0/scr/main/newuserdgamespes/vh2-r.lua' --[[function()
-local plug={
-esc={func=function()
-is_auto_enabled=not is_auto_enabled
-funcs.sendnotif("autoesc","status: "..tostring(is_auto_enabled),"rbxassetid://6678521436",5)
-end,desc="enable autoescape"};
-gg={func=function()
-local gn=workspace:FindFirstChild("gundropped")
-if gn then firetouchinterest(getchar():WaitForChild("HumanoidRootPart",20),gn,0) end end,desc="gungrab"};
-ag={func=function() gunt=not gunt
-funcs.sendnotif("auto-gun","status: "..tostring(gunt),"rbxassetid://6678521436",5) end,desc="autogg"}
---
-Reservedpluginname="VH2R"
-}
-
-return plug
-end--]]
+[10201298018]='https://raw.githubusercontent.com/exceptional0/scr/main/newuserdgamespes/vh2-r.lua';
+[198817751]='https://raw.githubusercontent.com/6yNuiC9/scr/main/newuserdgamespes/vh2.lua'
 }
 tb=tb[game.PlaceId]
-return tb and (type(tb)=='function' and tb() or type(tb)=='string' and loadstring(game:HttpGet(tb))() ) or tb
+if type(tb)=='function' then return tb() elseif type(tb)=='string' then return loadstring(game:HttpGet(tb))() end
+return tb
