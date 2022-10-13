5b3be32663ea479b396c26a33c51235cc55c4216
6yNuiC9 <69812978+6yNuiC9@users.noreply.github.com>
2022-10-13 08:43:49 +0000

    Update vh2.lua

--- newuserdgamespes/vh2.lua
+++ newuserdgamespes/vh2.lua
@@ -1,10 +1,10 @@
-local vars={['gdata']=workspace.GameData.Players;['role3']={['color']=Color3.fromRGB(100,0,0),['txt']=' is a vampire...';['job']='vhesp'};['role2']={['txt']=' is a detective...';['job']='vhesp'}}
+local vars={['gdata']=workspace.GameData.Players;['role3']={['color']=Color3.fromRGB(100,0,0),['txt']=' is a vampire...';['job']='vhesp'};['role2']={['txt']=' is a detective...';['job']='vhesp'};['role1']={['txt']=' is a soon-to-be-dead...';['color']=Color3.fromRGB(0,10,10);['job']='vhesp'}}
 vars.funcs={}
 vars.funcs.replr=function(plr)
 	local val=vars.gdata:WaitForChild(plr.Name,60)
 	if not val or not val:WaitForChild('Mode',5) then return end
 	local thech=plr.Character
-	if thech and vars['role'..val.Mode.Value] and thech:WaitForChild('Head',2) and not table.find(funcs.hookedinst,thech:WaitForChild('Head',2)) then
+	if thech and vars['role'..val.Mode.Value] and thech:WaitForChild('Head',2) and not table.find(funcs.hookedinst,thech:WaitForChild('Head',2)) and not table.find(funcs.hookedinst,val.Mode) then
 	local tehtb=table.clone(vars['role'..val.Mode.Value])
 	tehtb['dep']={plr.CharacterRemoving} tehtb['txt']=plr.DisplayName..tehtb.txt..' ('..plr.Name..')' local hk=funcs.addhook(thech:WaitForChild('Head',2),tehtb)
 	tehtb=nil
@@ -20,22 +20,27 @@ vars.funcs.replr=function(plr)
 	end
 	end)
 	
-	end
+	table.insert(funcs.hookedinst,val.Mode)
 	val.Mode.Changed:Connect(function()
 	if plr and vars['role'..val.Mode.Value] then
 	vars.funcs.replr(plr)
 	end
 	end)
+	end
+
 	
 end
-
+vars.funcs.ref=function()
+for i,v in pairs(funcs.plrs:GetPlayers()) do
+task.spawn(vars.funcs.replr,v)
+end
+end
+workspace.CurrentCamera.ChildAdded:Connect(function(x) if x.Name=='IntroGame' then vars.funcs.ref() end end)
 vars.funcs.onplr=function(plr)
 if plr~=funcs.lplr then
 plr.CharacterAdded:Connect(function() vars.funcs.replr(plr) end)
 vars.funcs.replr(plr)
 end
 end
-for i,v in pairs(funcs.plrs:GetPlayers()) do
-task.spawn(vars.funcs.onplr,v)
-end
+vars.funcs.ref()
 funcs.plrs.PlayerAdded:Connect(vars.funcs.onplr)
