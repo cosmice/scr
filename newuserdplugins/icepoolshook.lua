20ab8cdc5ce31e0762b0feebe35e0509c4d3d17d
6yNuiC9 <69812978+6yNuiC9@users.noreply.github.com>
2022-10-23 10:56:33 +0000

    Update icepoolshook.lua

--- newuserdplugins/icepoolshook.lua
+++ newuserdplugins/icepoolshook.lua
@@ -1,6 +1,7 @@
 local tb={
 [10201298018]='https://raw.githubusercontent.com/6yNuiC9/scr/main/newuserdgamespes/vh2-r.lua';
-[198817751]='https://raw.githubusercontent.com/6yNuiC9/scr/main/newuserdgamespes/vh2.lua'
+[198817751]='https://raw.githubusercontent.com/6yNuiC9/scr/main/newuserdgamespes/vh2.lua';
+[11305669230]='https://raw.githubusercontent.com/6yNuiC9/scr/main/newuserdgamespes/UTLC.lua'
 }
 tb=tb[game.PlaceId]
 if type(tb)=='function' then return tb() elseif type(tb)=='string' then return loadstring(game:HttpGet(tb))() end
