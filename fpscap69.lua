ecccbcc4dd899ab2af0d30e86a0fd52699b03b1e
6yNuiC9 <69812978+6yNuiC9@users.noreply.github.com>
2022-10-25 09:38:47 +0000

    Update fpscap69.lua

--- fpscap69.lua
+++ fpscap69.lua
@@ -1,16 +1,17 @@
 getgenv().fpsthingenabled = true
 getgenv().fpsthing = {}
 getgenv().fpsthing.maxfpswhen = 65
-getgenv().fpsthing.minfpswhen = 22
+getgenv().fpsthing.minfpswhen = 35
 setfpscap(fpsthing.maxfpswhen)
-local oldsetts={}
+local oldsetts={settings().Rendering.QualityLevel}
 local uis = game:GetService("UserInputService")
 local con1,con2 = nil,nil
 local ru=game:GetService("RunService")
+local settings=clonefunction(settings)
 con1 = uis.WindowFocused:Connect(function()
-	if getgenv().fpsthingenabled == true and #oldsetts>=2 then
-	settings().Rendering.QualityLevel=oldsetts[2]
-	settings().Physics.PhysicsEnvironmentalThrottle=oldsetts[1]
+	if getgenv().fpsthingenabled == true then
+	settings().Rendering.QualityLevel=oldsetts[1]
+	--settings().Physics.PhysicsEnvironmentalThrottle=oldsetts[1]
 	ru:Set3dRenderingEnabled(true)
     setfpscap(getgenv().fpsthing.maxfpswhen)
 	--[[else
@@ -19,14 +20,13 @@ con1 = uis.WindowFocused:Connect(function()
 	con1,con2=nil,nil--]]
 	end
 end)
-task.wait(2.5)
 con2 = uis.WindowFocusReleased:Connect(function() 
 	if getgenv().fpsthingenabled == true then
     setfpscap(getgenv().fpsthing.minfpswhen)
 	ru:Set3dRenderingEnabled(false)
-	oldsetts[1]=settings().Physics.PhysicsEnvironmentalThrottle
-	settings().Physics.PhysicsEnvironmentalThrottle = 1
-	oldsetts[2]=settings().Rendering.QualityLevel
+	--oldsetts[1]=settings().Physics.PhysicsEnvironmentalThrottle
+	--settings().Physics.PhysicsEnvironmentalThrottle = 1
+	oldsetts[1]=settings().Rendering.QualityLevel
 	settings().Rendering.QualityLevel = 'Level01'
 	--[[else
 	con1:Disconnect()
