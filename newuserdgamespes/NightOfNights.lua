cab7e7b87c16fdb195ba7419f6d652889bc6cc4e
6yNuiC9 <69812978+6yNuiC9@users.noreply.github.com>
2022-10-29 13:57:27 +0000

    Update NightOfNights.lua

--- newuserdgamespes/NightOfNights.lua
+++ newuserdgamespes/NightOfNights.lua
@@ -2,11 +2,13 @@ local vars={['rev']=game:GetService('ReplicatedStorage'):WaitForChild("Ragdoll",
 local plug={
 ['stam']={['func']=function()
 getchar():SetAttribute('StaminaRegenerationRate',10^5)
+funcs.sendnotif('NightOfNights\\bamb','stamina regen big','rbxassetid://8119590978',5)
 end;['desc']='instantly regen stamina'};
 ['ltesp']={['func']=function()
 if not workspace:FindFirstChild('Map') then funcs.sendnotif('nomap','nomap','',5) return end
 for i,v in pairs(game:GetService("Workspace").Map.Scripted.Safes:GetChildren()) do funcs.addhook(v) end
 for i,v in pairs(game:GetService("Workspace").Map.Scripted.Misc:GetChildren()) do funcs.addhook(v) end
+funcs.sendnotif('NightOfNights\\bamb','ltesp','rbxassetid://8119590978',5)
 end;['desc']='esp stuff'};
 ['rev']={['func']=function(a,aa)
 aa=aa and funcs.xgetplr(aa,true)
@@ -16,6 +18,7 @@ vars.rev:FireServer(aa,'Hold')
 vars.rev:FireServer(aa)
 vars.rev:FireServer(aa,'StopHold')
 vars.rev:FireServer(aa)
+funcs.sendnotif('NightOfNights\\revive','rev: '..aa.DisplayName,'rbxassetid://8119590978',5)
 end
 end;['desc']='revive plr'};
 ['Reservedpluginname']='Night Of Nights'
