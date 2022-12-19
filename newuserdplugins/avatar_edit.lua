ca9fde809a7d593effb3bdb67d137b661c8c208f
harsh noise gothic <69812978+6yNuiC9@users.noreply.github.com>
2022-12-19 12:36:22 +0000

    Update avatar_edit.lua

--- newuserdplugins/avatar_edit.lua
+++ newuserdplugins/avatar_edit.lua
@@ -1,14 +1,12 @@
 local avs=game:GetService("AvatarEditorService")
-
 local function settype(strd,plrarg,x,cmd)
 local hn=funcs.plrs:GetHumanoidDescriptionFromUserId(funcs.lplr.UserId)
 avs:PromptSaveAvatar(hn,Enum.HumanoidRigType[string.upper(cmd)])
 funcs.deb:AddItem(hn,0)
 end
-
 local plug={
-["r6"]={func=settype};
-Reservedpluginname="base.avatar_edit"
+["r6"]={['func']=settype};
+['Reservedpluginname']="base.avatar_edit"
 }
 plug.r15=plug.r6
 return plug
