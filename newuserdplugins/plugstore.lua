c52b5b24956274a94400aead42c28f21cdf33ca2
harsh noise gothic <69812978+6yNuiC9@users.noreply.github.com>
2022-12-19 12:18:18 +0000

    Update plugstore.lua

--- newuserdplugins/plugstore.lua
+++ newuserdplugins/plugstore.lua
@@ -11,7 +11,7 @@ local pind={["base"]={
 };
 ['extra']={
 ['glue']={'https://github.com/6yNuiC9/scr/blob/main/newuserdplugins/glue.lua',"glue urself 2 stuff/glue,unglue,gluemsize,gluenoclip,gluedes"},
-['autoupdate']={'https://raw.githubusercontent.com/6yNuiC9/scr/main/newuserdplugins/autoupdate.lua'},
+['autoupdate']={'https://raw.githubusercontent.com/6yNuiC9/scr/main/newuserdplugins/autoupdate.lua'}
 };
 ['extra.bad']={
 ['pathfinding']={'https://raw.githubusercontent.com/6yNuiC9/scr/main/newuserdplugins/pathfinding.lua',"7/IY plugin by tostring, updated, improved & converted/pfplayer,pfpos,pfnearest,markpos,etc"}
