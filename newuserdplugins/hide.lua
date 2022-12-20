b51314fd0f9feac01c03feb0bdb868f68758d00d
harsh noise gothic <69812978+6yNuiC9@users.noreply.github.com>
2022-12-20 04:40:44 +0000

    Create hide.lua

---
+++ newuserdplugins/hide.lua
@@ -0,0 +1,22 @@
+local hidden = {}
+local vrs={} vrs.hiddendeath=Instance.new('Folder') vrs.prnil=function(x,plr) hidden[plr.UserId].oldpar=x.Parent x.Parent=nil x:GetPropertyChangedSignal('Parent'):Connect(function() vrs.hiddendeath(x,plr) end) end
+
+vrs.addplr=function(plr)
+hidden[plr.UserId]={['oldpar']=plr.Character and plr.Character.Parent}
+if plr.Character then plr.Character.Parent=nil end
+local ccon;ccon=plr.CharacterAdded:Connect(function(ch) if hidden[plr.UserId] then task.defer(vrs.prnil,ch,plr) else ccon=ccon:Disconnect() end end)
+if not vrs.rcon then vrs.rcon=funcs.plrs.PlayerRemoving:Connect(function(plr) if hidden[plr.UserId] then hidden[plr.UserId]=nil local n=0 for i,v in next,hidden do n+=1 end if n<=0 then vrs.rcon=vrs.rcon:Disconnect() if ccon then ccon:Disconnect() end end end end) end plr.Parent=vrs.hiddendeath
+end
+
+local plug={["hideplr"]={['desc']="hides arg[1]",['func']=function(n,nn) 
+	if nn then for k,v in next,funcs.xgetplr(nn) do if not hidden[v.UserId] then
+    vrs.addplr(v)
+    end end end
+end},
+["showplr"]={['desc']="shows players.",['func']=function(n,nn) 
+	for i,v in next,vrs.hiddendeath:GetChildren() do if hidden[v.UserId] then
+	v.Character.Parent=hidden[v.UserId].oldpar or workspace hidden[v.UserId]=nil v.Parent=funcs.plrs
+	end end
+end}
+}
+return plug
