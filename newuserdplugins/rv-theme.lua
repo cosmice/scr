fab62d46a785cb7cf50baaa37bf63754285f6ddf
harsh noise gothic <69812978+6yNuiC9@users.noreply.github.com>
2022-12-21 10:35:07 +0000

    Update rv-theme.lua

--- newuserdplugins/rv-theme.lua
+++ newuserdplugins/rv-theme.lua
@@ -1,16 +1,15 @@
 local function setatr(inst,props)
-for y,x in pairs(inst) do
-for i,v in pairs(props) do
+for y,x in next,inst do
+for i,v in next,props do
 x:SetAttribute(v,x[v])
 end
 end
-
 end
 powersupply.gnn._txtbox.BackgroundColor3=Color3.fromRGB(76, 25, 25)
 powersupply.gnn.cmdframe.BackgroundColor3=Color3.fromRGB(17,0,0)
 powersupply.gnn.cmdframe.BorderColor3=Color3.fromRGB(100,0,0)
-powersupply.gnn["_close"].BorderColor3=Color3.fromRGB(100,0,0)
-powersupply.gnn["_close"].BackgroundColor3=Color3.fromRGB(17,0,0)
+powersupply.gnn._close.BorderColor3=Color3.fromRGB(100,0,0)
+powersupply.gnn._close.BackgroundColor3=Color3.fromRGB(17,0,0)
 
-setatr({powersupply.gnn["_close"],powersupply.gnn.cmdframe,powersupply.gnn._txtbox},{'BorderColor','BorderColor3','BackgroundColor3','BackgroundColor'})
+setatr({powersupply.gnn._close,powersupply.gnn.cmdframe,powersupply.gnn._txtbox},{'BorderColor','BorderColor3','BackgroundColor3','BackgroundColor'})
 setatr=nil
