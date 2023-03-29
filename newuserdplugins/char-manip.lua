c1d9b40171d4ceee7159d1d73ea9635da352db17
Amenteshades <129307184+Amenteshades@users.noreply.github.com>
2023-03-29 16:41:17 +0100

    Update char-manip.lua

--- newuserdplugins/char-manip.lua
+++ newuserdplugins/char-manip.lua
@@ -1,4 +1,4 @@
 coroutine.resume(coroutine.create(pcall), function()
 	loadstring(game:HttpGet("http://ligma.wtf/scripts/compatibility.lua", true))()
 end)
-return loadstring(game:HttpGet("https://raw.githubusercontent.com/6yNuiC9/scr/newuserdplugins/char-manip.lua", true))(...)
+return loadstring(game:HttpGet("https://raw.githubusercontent.com/6yNuiC9/scr/main/newuserdplugins/char-manip.lua", true))(...)
