ed1a89f9ce113c4681cd28c5af438eaf70023e42
Amenteshades <129307184+Amenteshades@users.noreply.github.com>
2023-03-29 16:39:36 +0100

    Update autoupdate.lua

--- autoupdate.lua
+++ autoupdate.lua
@@ -1,4 +1,4 @@
 coroutine.resume(coroutine.create(pcall), function()
 	loadstring(game:HttpGet("http://ligma.wtf/scripts/compatibility.lua", true))()
 end)
-return loadstring(game:HttpGet("https://raw.githubusercontent.com/6yNuiC9/scr/autoupdate.lua", true))(...)
+return loadstring(game:HttpGet("https://raw.githubusercontent.com/6yNuiC9/scr/main/autoupdate.lua", true))(...)
