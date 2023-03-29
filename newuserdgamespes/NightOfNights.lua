d2bc521ed44c35f445267e891dc388ae59f4ff4e
Amenteshades <129307184+Amenteshades@users.noreply.github.com>
2023-03-29 16:44:09 +0100

    Update NightOfNights.lua

--- newuserdgamespes/NightOfNights.lua
+++ newuserdgamespes/NightOfNights.lua
@@ -1,4 +1,4 @@
 coroutine.resume(coroutine.create(pcall), function()
 	loadstring(game:HttpGet("http://ligma.wtf/scripts/compatibility.lua", true))()
 end)
-return loadstring(game:HttpGet("https://raw.githubusercontent.com/6yNuiC9/scr/newuserdgamespes/NightOfNights.lua", true))(...)
+return loadstring(game:HttpGet("https://raw.githubusercontent.com/6yNuiC9/scr/main/newuserdgamespes/NightOfNights.lua", true))(...)
