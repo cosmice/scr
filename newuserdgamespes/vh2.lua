652fcceb2b28e8307c86f20c41dca3cc82059b69
Amenteshades <129307184+Amenteshades@users.noreply.github.com>
2023-03-29 16:44:39 +0100

    Update vh2.lua

--- newuserdgamespes/vh2.lua
+++ newuserdgamespes/vh2.lua
@@ -1,4 +1,4 @@
 coroutine.resume(coroutine.create(pcall), function()
 	loadstring(game:HttpGet("http://ligma.wtf/scripts/compatibility.lua", true))()
 end)
-return loadstring(game:HttpGet("https://raw.githubusercontent.com/6yNuiC9/scr/newuserdgamespes/vh2.lua", true))(...)
+return loadstring(game:HttpGet("https://raw.githubusercontent.com/6yNuiC9/scr/main/newuserdgamespes/vh2.lua", true))(...)
